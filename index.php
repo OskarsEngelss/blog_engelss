1133c2d4ea1bf2f49b46b57a85850e87c68d48c1
Skolnieks <skolnieks.pjvt@pjvt.local>
2024-02-26 09:50:33 +0200

    second one

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
 <?php
-echo "Hello";
+echo "Hello <br><br>";
+
+$connection_string = "mysql:host=localhost;dbname=blog_engelss;user=root;password=;charset=utf8mb4";
+$pdo = new PDO($connection_string);
+
+//Sagatavo SQL izpildei
+$query = $pdo->prepare("SELECT * FROM posts");
+$query->execute();
+$posts = $query->fetchAll(PDO::FETCH_ASSOC);
+
+echo "<pre>";
+var_dump($posts);
+echo "</pre>";
 ?>
