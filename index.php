d5f50e1d613c6c5dd8e12c79f671a1bc405db44e
Skolnieks <skolnieks.pjvt@pjvt.local>
2024-02-27 13:27:28 +0200

    third commit

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
+require "helpers.php";
+
 echo "Hello <br><br>";
 
 $connection_string = "mysql:host=localhost;dbname=blog_engelss;user=root;password=;charset=utf8mb4";
@@ -9,7 +11,9 @@ $query = $pdo->prepare("SELECT * FROM posts");
 $query->execute();
 $posts = $query->fetchAll(PDO::FETCH_ASSOC);
 
-echo "<pre>";
-var_dump($posts);
-echo "</pre>";
+echo "<ol>";
+    foreach($posts as $post) {
+        echo "<li>" . $post["title"] . "</li>";
+    }
+echo "</ol>";
 ?>
