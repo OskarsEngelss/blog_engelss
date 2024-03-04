ca428242ade7d5af9f0335d5efa389e51202b272
Skolnieks <skolnieks.pjvt@pjvt.local>
2024-03-04 15:17:45 +0200

    added $_GET

--- index.php
+++ index.php
@@ -5,14 +5,24 @@ require "helpers.php";
 require "Database.php";
 $config = require("config.php");
 
+$query = "SELECT * FROM posts";
+if (isset($_GET["id"]) && $_GET["id"]!=null) {
+    $id = $_GET["id"];
+    $query = "SELECT * FROM posts WHERE id=$id";
+}
 $db = new Database($config);
 $posts = $db
-        ->execute("SELECT * FROM posts")
+        ->execute($query)
         ->fetchAll();
 
-echo "<ol>";
+echo "<form>";
+echo "<input name='id' />";
+echo "<button>Submit</button>";
+echo "</form>";
+
+echo "<ul>";
     foreach($posts as $post) {
         echo "<li>" . $post["title"] . "</li>";
     }
-echo "</ol>";
+echo "</ul>";
 ?>
